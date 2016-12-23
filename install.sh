94e7cdc700bc3d6355fee3dd060be02b38a8f926
juliusbuckley <juliusbuckley@gmail.com>
2016-12-23 16:22:43 -0500

    chores/ if bable-cli is not installed globally  it will be installed

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #  This script should do all the preparation for your project to run, such as downloading any dependencies and compiling if necessary
 echo "Installing dependencies..."
 npm install
-npm install -g babel-cli
+if test -s /usr/bin/babel
+then
+  echo "babel-cli has already been installed globally!"
+else
+  echo "Installing babel-cli globally..."
+  npm install -g babel-cli
+fi
 wait
 echo "Install complete!"
 echo "Transpiling..."
