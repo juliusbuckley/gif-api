af64e888454ef469149becd79167ce99048b6226
juliusbuckley <juliusbuckley@gmail.com>
2016-12-23 13:49:53 -0500

    chores/ added transipiling echo

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #  This script should do all the preparation for your project to run, such as downloading any dependencies and compiling if necessary
-echo "Installing dependencies and transpiling..."
+echo "Installing dependencies..."
 npm install
 wait
 echo "Install complete!"
+echo "Transpiling..."
 babel ./server -d ./server-dist --presets es2015,stage-2 --copy-files
 wait
 echo "Transpiling complete!"
