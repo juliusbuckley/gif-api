a5eaff214c93ad5b8f2e39536f35dfa905c315a1
juliusbuckley <juliusbuckley@gmail.com>
2016-12-23 10:34:30 -0500

    feat/ created search query and http request to giphy api

--- server/routes/gifRoutes.js
+++ server/routes/gifRoutes.js
@@ -1,8 +1,25 @@
+import axios from 'axios';
+import qs from 'querystring';
 // API routes
 const gifRoutes = (app) => {
   app.get('/search/:searchTerm', (req, res) => {
-    // integrate with gify API
+    const searchUrl = 'http://api.giphy.com/v1/gifs/search';
+    const parameters = {
+      q: req.params.searchTerm,
+      api_key: process.env.GIPHY_KEY,
+      limit: 5
+    };
+    const paramUrl = qs.stringify(parameters);
+    const apiUrl = searchUrl + '?' + paramUrl;
+    axios.get(apiUrl)
+      .then((response) => {
+        if (response.data.pagination.total_count < 5) {
+          res.send('Sorry, your search had less than 5 results and cannot be displayed!');
+        } else {
+          res.send(response.data);
+        }
+      })
+      .catch((err) => { `Error: ${err}`; });
   });
 };
-
 export default gifRoutes;
