aa78c9213b9f6ddf70e865d1dede278ba05015ad
juliusbuckley <juliusbuckley@gmail.com>
2016-12-23 11:18:03 -0500

    feat/ created function to parse response

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
 import qs from 'querystring';
 
-const getGiphys = (req, res) => {
+// PARSE RESPONSE DATA
+const parseResp = (giphyResp) => {
+  const array = [];
+  giphyResp.data.forEach((giphy) => {
+    array.push({
+      gif_id: giphy.id,
+      url: giphy.url
+    });
+  });
+  return {data: array};
+};
+
+// CREATE QUERY STRING AND MAKE REQUEST TO GIPHY API
+const getGiphy = (req, res) => {
   const searchUrl = 'http://api.giphy.com/v1/gifs/search';
   const parameters = {
     q: req.params.searchTerm,
@@ -15,10 +28,11 @@ const getGiphys = (req, res) => {
       if (response.data.pagination.total_count < 5) {
         res.send('Sorry, your search had less than 5 results and cannot be displayed!');
       } else {
-        res.send(response.data);
+        const data = parseResp(response.data);
+        res.json(data);
       }
     })
     .catch((err) => { `Error: ${err}`; });
 };
 
-export default getGiphys;
+export default getGiphy;
