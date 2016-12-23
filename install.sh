1f1315a7700860e64f3d06ce68e23621f6199693
juliusbuckley <juliusbuckley@gmail.com>
2016-12-23 13:32:59 -0500

    chores/ completed install and start script

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 #  This script should do all the preparation for your project to run, such as downloading any dependencies and compiling if necessary
+echo "Installing dependencies and transpiling..."
+npm install
+wait
+echo "Install complete!"
+babel ./server -d ./server-dist --presets es2015,stage-2 --copy-files
+wait
+echo "Transpiling complete!"
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 #  This script should start up your service on port 8080
+node ./server-dist/app.js
